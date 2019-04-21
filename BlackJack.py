67cc21ce3df37ec8107dadaf6aaed58cec8a97a8
Cam Deatherage <41445364+ctd1077@users.noreply.github.com>
2019-04-20 21:02:57 -0600

    latest update

--- BlackJack.py
+++ BlackJack.py
@@ -8,7 +8,8 @@ import time
 
 
 suit = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
-cards = pd.Series(['2','3','4','5','6','7','8','9','10','J','Q','K','A'])
+cards = pd.Series(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q',
+                  'K', 'A'])
 
 
 class Player():
@@ -16,6 +17,7 @@ class Player():
     def __init__(self):
 
         self.score = 0
+
 
 class HumanPlayer(Player):
     '''Child Class HumanPlayer'''
@@ -31,6 +33,7 @@ class HumanPlayer(Player):
         self.score += point
         return point
 
+
 class Dealer(Player):
     '''Child Class ComputerPlayer'''
     def __init__(self):
@@ -45,12 +48,14 @@ class Dealer(Player):
         self.score += point
         return point
 
+
 def new_deck():
         '''A fucntion to create a new deck of cards'''
         # deck = pd.concat({'Hearts':Hearts,'Spades':Spades,'Clubs':Clubs,
         # 'Diamonds':Diamonds}, axis=1)
         lst = []
         return lst
+
 
 def deal_Card(lst):
     ''' This function deals a card, one card at a time
@@ -70,11 +75,11 @@ def deal_Card(lst):
                 break
             continue
         else:
-            lst.append((select_suit,card))
-            #deck.loc[deck[select_suit] == card, select_suit] = None
+            lst.append((select_suit, card))
             played = True
             break
     return card, select_suit
+
 
 def points(card):
     ''' This function decides if the card varible can be dtype of int
@@ -92,11 +97,8 @@ def points(card):
             point = 10
     return point
 
+
 class Game():
-
-    #player_cards = []
-    #dealer_cards = []
-
 
     def __init__(self):
         self.p1 = HumanPlayer()
@@ -105,11 +107,11 @@ class Game():
         self.dealer_cards = []
 
     def start_Game(self):
-        game = True
 
         p1 = self.p1.deal(lst)
         self.player_cards.append(p1)
         self.p1.point(card)
+        time.sleep(2)
         print('Player gets the first card: ', self.player_cards)
         p2 = self.p2.deal(lst)
         self.dealer_cards.append(p2)
@@ -126,48 +128,48 @@ class Game():
         self.p2.point(card)
         time.sleep(2)
         print('Dealer gets second card: ', self.dealer_cards[1])
+        win = False
         if self.p1.score == 22:
             print('Two aces will be played as a soft hand:')
             self.p1.score == 12
         elif self.p1.score == 21:
+            print('BlackJack!')
             print('Congradulations you win!')
-
-
+            win = True
         time.sleep(2)
-        print('Your hand is : ', self.player_cards, 'for a total of ', self.p1.score, 'points')
-        time.sleep(2)
+        print('Your hand is : ', self.player_cards, 'for a total of ',
+              self.p1.score, 'points')
+        time.sleep(1)
         print('The only card the Dealer shows is :', self.dealer_cards[1])
-# right now it only ask the play for another card once through the loop
+        return win
+
     def player_Round(self):
         while self.p1.score <= 21:
-            time.sleep(2)
+            time.sleep(1)
             ans = input('Would you like another card? [Y]es or [N]o? ')
+            win = False
             if ans == 'y':
                 p1 = self.p1.deal(lst)
                 self.p1.point(card)
                 self.player_cards.append(p1)
                 if self.p1.score <= 21:
                     print('Your card is: ', p1,)
-                    print('Hand: ', self.player_cards, 'Total points', self.p1.score)
-                    p1 = self.p1.deal(lst)
-                    self.p1.point(card)
-                    self.player_cards.append(p1)
+                    print('Hand: ', self.player_cards, 'Total points',
+                          self.p1.score)
+                    win = False
                     continue
                 elif self.p1.score > 21:
                     print('Your card is: ', p1)
                     time.sleep(2)
                     print('Sorry you bust and dealer wins')
-                    time.sleep(2)
-                    print('Game Over!')
+                    win = True
                     break
             else:
                 break
+        return win
 
     def dealer_Round(self):
         print('Dealer shows his cards: ', self.dealer_cards)
-        #p2 = self.p2.deal(lst)
-        #self.dealer_cards.append(p2)
-        #print('Dealers card is: ', p2)
         game = True
         if self.p2.score == 22:
             time.sleep(2)
@@ -176,7 +178,7 @@ class Game():
         elif self.p2.score == 21:
             print('21! Dealer wins!')
             game = False
-
+            win = True
         while game is True:
             if self.p2.score <= 16:
                 print('Dealer takes another card: ')
@@ -186,24 +188,36 @@ class Game():
                 time.sleep(2)
                 print('Dealers card is: ', p2)
                 game = True
+                win = False
                 continue
             elif self.p2.score == 21:
                 time.sleep(2)
                 print('Dealer has 21 points')
                 print('Sorry you lose!')
                 game = False
+                win = True
                 break
             # Try using just a else statement and see what happens
             elif self.p2.score > 21:
                 time.sleep(2)
                 print('Dealer has bust!')
-                print('YOU WIN!')
+                print('You Win!')
                 game = False
+                win = True
                 break
             else:
                 time.sleep(2)
-                print('Dealer stays with ',self.p2.score ,' points')
+                print('Dealer stays with ', self.p2.score, ' points')
+                win = False
                 break
+        return win
+
+    def wl(self):
+        if self.p1.score <= self.p2.score:
+            print('You Lose!')
+        else:
+            print('You Win!')
+
 
 if __name__ == '__main__':
     answer = input('Welcome to Blackjack!\n Would you like to play a game?\n\
@@ -211,18 +225,23 @@ if __name__ == '__main__':
     answer = answer.upper()
 
     while answer != 'N':
-        print('Great and good luck!\n')
+        print('Good luck!\n')
         lst = new_deck()
-        time.sleep(2)
         print('Dealer shuffles the deck\n')
         game = Game()
         game.start_Game()
-        game.player_Round()
-        game.dealer_Round()
-
+        win = game.player_Round()
+        if win is False:
+            win = game.dealer_Round()
+            if win is True:
+                print('Game Over!')
+            else:
+                game.wl()
+        else:
+            print('Game Over!')
 
         answer = input('Would you like to play another game:')
         answer = answer.upper()
-        if answer  is 'N':
+        if answer is 'N':
             break
     print('Thank you and have a great day.')
